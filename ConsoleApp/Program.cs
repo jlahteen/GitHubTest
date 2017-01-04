530b8d1cd8c57f00c8c564470c51910766dad6d4
lahtejuh <lahtejuh@WL315046>
2017-01-04 13:26:10 +0200

    Latest version output added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp
             if (args.Length != 0)
                 return;
 
-            Console.WriteLine("Hello Github!");
+            Console.WriteLine("Hello Github! Latest version");
         }
     }
 }
